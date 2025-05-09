40e7e256eb196aae4c187bce5cc1cfcd7dca4d57
Mayderson Mello <maydersonmello@gmail.com>
2025-05-09 19:18:09 -0300

    create constants

---
+++ src/constants/base-media-path.ts
@@ -0,0 +1,3 @@
+import { resolve } from 'node:path';
+
+export const BASE_MEDIA_PATH = resolve(__dirname, '..', '..', 'medias');
---
+++ src/constants/queues.ts
@@ -0,0 +1,5 @@
+export enum QUEUES {
+  EXTRACT_AUDIO_QUEUE = 'extract-audio-queue',
+  GENERATE_SUBTITLES_QUEUE = 'generate-subtitles-queue',
+  ADD_SUBTITLES_QUEUE = 'add-subtitles-queue',
+}
