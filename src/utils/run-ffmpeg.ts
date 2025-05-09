642e8c1e149980541f2eb0b0b0e9510e39c82f93
Mayderson Mello <maydersonmello@gmail.com>
2025-05-09 19:17:24 -0300

    create function to run ffmpeg

---
+++ src/utils/run-ffmpeg.ts
@@ -0,0 +1,16 @@
+import { spawn } from 'node:child_process';
+
+export async function runFfmpeg(args: string[]): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const ffmpeg = spawn('ffmpeg', args, { stdio: 'inherit' });
+
+    ffmpeg.on('error', (error) => reject(error));
+    ffmpeg.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`FFmpeg finished with code ${code}`));
+      }
+    });
+  });
+}
