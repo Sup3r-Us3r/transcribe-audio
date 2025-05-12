38fb318ceb81e30bcdb944b746a238acedd9b9c2
Mayderson Mello <maydersonmello@gmail.com>
2025-05-12 19:31:07 -0300

    increase fastify multipart limits to 800MB

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,11 @@ import { videoRoutes } from './http/controllers/video/routes';
 
 const app = fastify({ logger: true });
 
-app.register(fastifyMultipart);
+app.register(fastifyMultipart, {
+  limits: {
+    fileSize: 800 * 1024 * 1024, // 800MB
+  },
+});
 app.register(videoRoutes);
 
 export { app };
