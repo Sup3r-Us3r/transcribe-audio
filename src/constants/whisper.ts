7c475bb32aa98ab18bc4b5b770d9eb32b4c66ec8
Mayderson Mello <maydersonmello@gmail.com>
2025-05-14 20:27:18 -0300

    change whisper language to portuguese

--- src/constants/whisper.ts
+++ src/constants/whisper.ts
@@ -29,4 +29,4 @@ export const WHISPER_MODEL: WhisperModel = 'medium';
 // Language to transcribe
 // If you set another language than 'en', remove .en from the WHISPER_MODEL
 // List of languages: https://github.com/openai/whisper/blob/main/whisper/tokenizer.py
-export const WHISPER_LANG: Language = 'en';
+export const WHISPER_LANG: Language = 'pt';
