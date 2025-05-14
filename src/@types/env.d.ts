bb81765704d7b5c0f21a43513cb483660e57c8fb
Mayderson Mello <maydersonmello@gmail.com>
2025-05-14 09:47:13 -0300

    add type to process.env

---
+++ src/@types/env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+    WEBHOOK_URL: string;
+  }
+}
