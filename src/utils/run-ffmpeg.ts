7b104078f8221666726992608a1a0d74e3474979
Mayderson Mello <maydersonmello@gmail.com>
2025-06-10 19:11:01 -0300

    hide ffmpeg verbose output

--- src/utils/run-ffmpeg.ts
+++ src/utils/run-ffmpeg.ts
@@ -2,7 +2,7 @@ import { spawn } from 'node:child_process';
 
 export async function runFfmpeg(args: string[]): Promise<void> {
   return new Promise((resolve, reject) => {
-    const ffmpeg = spawn('ffmpeg', args, { stdio: 'inherit' });
+    const ffmpeg = spawn('ffmpeg', args, { stdio: ['ignore', 'inherit'] });
 
     ffmpeg.on('error', (error) => reject(error));
     ffmpeg.on('close', (code) => {
